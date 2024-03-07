fffed7d94ae3cfb9cbdda29e2102b0bb05c617fb
Tonmoy01 <mdtonmoykhan65@gmail.com>
2024-03-07 19:26:34 +0600

    added debounce in searching system

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,4 @@
-// Search.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchTerm } from '../store/slice/searchSlice';
 
@@ -7,8 +6,25 @@ function Search() {
   const dispatch = useDispatch();
   const searchTerm = useSelector((state) => state.search);
 
+  const [inputValue, setInputValue] = useState('');
+
+  const [debounceTimer, setDebounceTimer] = useState(null);
+
+  const debounce = (func, delay) => {
+    if (debounceTimer) {
+      clearTimeout(debounceTimer);
+    }
+    const timer = setTimeout(func, delay);
+    setDebounceTimer(timer);
+  };
+
   const handleSearch = (e) => {
-    dispatch(setSearchTerm(e.target.value));
+    const value = e.target.value;
+    setInputValue(value);
+
+    debounce(() => {
+      dispatch(setSearchTerm(value));
+    }, 300);
   };
 
   return (
@@ -17,7 +33,7 @@ function Search() {
         type='search'
         className='w-full outline-none'
         placeholder='Search your todo'
-        value={searchTerm}
+        value={inputValue}
         onChange={handleSearch}
       />
       <button className='p-1 text-white rounded-full text-md bg-cyan-500'>
